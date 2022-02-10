7eda798e9281e7b9876cdf1bc16c708ddf3ceeb8
Emre Onur <emre.onur@atrosbt.com>
2022-02-10 22:37:48 +0300

    I added new error message

--- MessengerApp.Application/Constants/DbServiceErrorMessages.cs
+++ MessengerApp.Application/Constants/DbServiceErrorMessages.cs
@@ -2,6 +2,7 @@ namespace MessengerApp.Application.Constants;
 
 public static class DbServiceErrorMessages
 {
-    public const string NotFoundData = "Not found data";
-    public const string NotFoundDataById = "Not found data by id";
+    public const string NotFoundData = "Not found data.";
+    public const string NotFoundDataById = "Not found data by id.";
+    public const string NotFoundDataByPhoneNumber = "Not found message by sender or receiver phone number.";
 }
