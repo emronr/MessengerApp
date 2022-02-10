e57b25f1fabc86581aa0c2ec065f3cd0549cfa18
Emre Onur <emre.onur@atrosbt.com>
2022-02-10 22:30:19 +0300

    I added DbService

---
+++ MessengerApp.Application/Constants/DbServiceErrorMessages.cs
@@ -0,0 +1,7 @@
+namespace MessengerApp.Application.Constants;
+
+public static class DbServiceErrorMessages
+{
+    public const string NotFoundData = "Not found data";
+    public const string NotFoundDataById = "Not found data by id";
+}
---
+++ MessengerApp.Persistence/Services/DbService.cs
@@ -0,0 +1,69 @@
+using System.Net;
+using MessengerApp.Application.Constants;
+using MessengerApp.Application.Errors;
+using MessengerApp.Application.Exception;
+using MessengerApp.Application.Interfaces.Services;
+using MessengerApp.Application.RequestResponseModels;
+using MessengerApp.Application.RequestResponseModels.RequestModels;
+using MessengerApp.Application.RequestResponseModels.ResponseModels;
+using MessengerApp.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessengerApp.Persistence.Services;
+
+public class DbService<TEntity> : IDbServices<TEntity>
+    where TEntity : class
+{
+    private readonly ApplicationDbContext _context;
+
+    public virtual DbSet<TEntity> _DbSet => _context.Set<TEntity>();
+
+    public DbService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<BaseResponse<TEntity>> AddAsync(TEntity entity)
+    {
+        await _context.AddAsync(entity);
+        await _context.SaveChangesAsync();
+
+        return new BaseResponse<TEntity>(entity);
+    }
+
+    public async Task Delete(TEntity entity)
+    {
+        _context.Entry(entity).State = EntityState.Deleted;
+        _context.Remove(entity);
+
+        await Task.CompletedTask;
+    }
+
+    public async Task<BaseResponse<TEntity>> UpdateAsync(TEntity entity)
+    {
+        _context.Entry(entity).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        
+        return new BaseResponse<TEntity>(entity);
+    }
+
+    public async Task<BaseResponse<TEntity>> GetById(int id)
+    {
+        var data = await _DbSet.FindAsync(id);
+        if (data == null)
+            throw new UserFriendlyException(DbServiceErrorMessages.NotFoundDataById, HttpStatusCode.NoContent);
+                
+        return new BaseResponse<TEntity>(data);
+    }
+
+    public async Task<BaseResponse<IPaginate<TEntity>>> GetAllAsync(QueryParameter<TEntity> queryParameter)
+    {
+        var datas = _DbSet.AsQueryable();
+        if(datas == null)
+            throw new UserFriendlyException(DbServiceErrorMessages.NotFoundData, HttpStatusCode.NoContent);
+
+        var paginatedDatas = new Paginate<TEntity>(datas, queryParameter);
+        
+        return new BaseResponse<IPaginate<TEntity>>(paginatedDatas);
+    }
+}
