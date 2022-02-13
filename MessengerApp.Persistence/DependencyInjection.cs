51faf15d889406305c98578defa3f8c12603c46b
Emre Onur <emre.onur@atrosbt.com>
2022-02-13 23:02:30 +0300

    I added depends in persistence

--- MessengerApp.Persistence/DependencyInjection.cs
+++ MessengerApp.Persistence/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using MessengerApp.Application.Interfaces.Context;
 using MessengerApp.Application.Interfaces.Services;
+using MessengerApp.Domain.Entities;
 using MessengerApp.Persistence.Context;
 using MessengerApp.Persistence.Services;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,7 +12,9 @@ public static class DependencyInjection
     public static IServiceCollection AddPersistence(this IServiceCollection services)
     {
         services.AddScoped<IApplicationDbContext,ApplicationDbContext>();
-        services.AddScoped(typeof(IDbServices<>),typeof(DbService<>));
+        services.AddScoped<ICustomerServices,ICustomerDbService>();
+        services.AddScoped<IMessageInfoServices, MessageInfoDbService>();
+
         return services;
     }
 }
